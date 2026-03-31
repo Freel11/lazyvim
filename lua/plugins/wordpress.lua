83f60fe1d83e28652f72f84b7fa4a2d20478dd3f
Jade Freel <jfreel@Mac-92.lan>
2026-03-30 22:28:28 -0400

    Updated config to conform with cl standards

--- lua/plugins/wordpress.lua
+++ lua/plugins/wordpress.lua
@@ -1,32 +1,43 @@
--- Define the path to your Global Composer Binaries
--- Based on your logs, this is where they live:
-local composer_bin = "C:/Users/jfreel/AppData/Roaming/Composer/vendor/bin"
+-- Helper function to prefer project-local binaries over global ones
+local function get_binary_path(bin_name)
+  local local_bin = vim.fn.getcwd() .. "/vendor/bin/" .. bin_name
+  if vim.fn.executable(local_bin) == 1 then
+    return local_bin
+  end
+  return vim.fn.expand("~/.config/composer/vendor/bin/" .. bin_name)
+end
 
 return {
-  -- 1. Mason: STOP installing phpcs/phpcbf here.
-  -- We want to use the global ones that have the correct dependencies.
+  -- 1. Mason: STOP installing PHP tools here.
   {
     "mason-org/mason.nvim",
     opts = {
-      ensure_installed = {
-        -- "phpcs",  <-- Removed
-        -- "phpcbf", <-- Removed
-      },
+      ensure_installed = {},
     },
   },
 
-  -- 2. Nvim-Lint: Point to the Composer phpcs
+  -- 2. Nvim-Lint: Point to local/global binaries
   {
     "mfussenegger/nvim-lint",
     opts = {
       linters_by_ft = {
-        php = { "phpcs" },
+        php = { "phpcs", "phpmd", "phpstan" },
       },
       linters = {
         phpcs = {
-          -- cmd needs to be a function or string.
-          -- On Windows, Composer usually creates a .bat file.
-          cmd = composer_bin .. "/phpcs.bat",
+          cmd = function()
+            return get_binary_path("phpcs")
+          end,
+        },
+        phpmd = {
+          cmd = function()
+            return get_binary_path("phpmd")
+          end,
+        },
+        phpstan = {
+          cmd = function()
+            return get_binary_path("phpstan")
+          end,
         },
       },
     },
@@ -41,11 +52,31 @@ return {
       },
       formatters = {
         phpcbf = {
-          -- Point to the global composer executable
-          command = composer_bin .. "/phpcbf.bat",
-
-          prepend_args = { "--standard=WordPress" },
+          command = function()
+            return get_binary_path("phpcbf")
+          end,
           valid_exit_codes = { 0, 1 },
+        },
+      },
+    },
+  },
+
+  -- 4. LSP Config: Configure Intelephense to read ACF Stubs
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        phpactor = false,
+        intelephense = {
+          settings = {
+            intelephense = {
+              environment = {
+                includePaths = {
+                  vim.fn.expand("~/.config/composer/vendor/php-stubs/acf-pro-stubs"),
+                },
+              },
+            },
+          },
         },
       },
     },
